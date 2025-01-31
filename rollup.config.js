06f4e0b82bc93386b08beaf4f72812f28fe62dc7
Matias Benary <matiasbenary@gmail.com>
2025-01-31 20:31:01 -0300

    useCounter

---
+++ rollup.config.js
@@ -0,0 +1,27 @@
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import babel from '@rollup/plugin-babel';
+import peerDepsExternal from 'rollup-plugin-peer-deps-external';
+
+export default {
+  input: "src/index.js",
+  output: [
+    {
+      file: 'dist/index.cjs.js',
+      format: 'cjs'
+    },
+    {
+      file: 'dist/index.esm.js',
+      format: 'esm'
+    }
+  ],
+  plugins: [
+    peerDepsExternal(),
+    resolve(),
+    commonjs(),
+    babel({
+      babelHelpers: 'bundled',
+      exclude: 'node_modules/**'
+    })
+  ]
+};
