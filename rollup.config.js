a1485f97e9991d2605cf7fe60d50d113df39efe2
Matias Benary <matiasbenary@gmail.com>
2025-02-01 07:58:56 -0300

    feat: chaged files from js to ts

--- rollup.config.js
+++ rollup.config.js
@@ -2,9 +2,10 @@ import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import babel from '@rollup/plugin-babel';
 import peerDepsExternal from 'rollup-plugin-peer-deps-external';
+import typescript from "@rollup/plugin-typescript";
 
 export default {
-  input: "src/index.js",
+  input: "src/index.ts",
   output: [
     {
       file: 'dist/index.cjs.js',
@@ -19,6 +20,7 @@ export default {
     peerDepsExternal(),
     resolve(),
     commonjs(),
+    typescript(),
     babel({
       babelHelpers: 'bundled',
       exclude: 'node_modules/**'
